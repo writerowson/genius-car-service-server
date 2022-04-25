1598ff4c85654ab86fb64cb7b5c0c00a93a684f1
coderowson <runurowson@gmail.com>
2022-04-25 20:45:19 +0600

    secure password

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
-const port = proccess.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
 const app = express()
 
 // middlware
 app.use(cors());
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofq71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('genius');
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('running genius server')
 })
 
-// to llisten 5000 port in browser
+// to listen 5000 port in browser
 app.listen(port, () => {
     console.log('listening to the port', port);
 })
