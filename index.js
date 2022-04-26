1f2277b5aa4591a6a03df8595248b1fcd8efd9b3
coderowson <runurowson@gmail.com>
2022-04-26 08:09:08 +0600

    load mongo data

--- index.js
+++ index.js
@@ -13,12 +13,24 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofq71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('genius');
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniuscarservice').collection('service');
+        // to load data
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
